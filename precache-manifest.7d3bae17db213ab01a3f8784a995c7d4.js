01b9834a5223f787138d4bd0f87ee5ed83abb0e7
Timurmurmur <bartimurmurmur@gmail.com>
2020-05-01 22:32:52 +0300

    Updates

---
+++ precache-manifest.7d3bae17db213ab01a3f8784a995c7d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a0507838ba28fbd027371d70e56741",
+    "url": "/foodFetish/index.html"
+  },
+  {
+    "revision": "62ee0f5000dcd15d8656",
+    "url": "/foodFetish/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "353a400376100f25f880",
+    "url": "/foodFetish/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "62ee0f5000dcd15d8656",
+    "url": "/foodFetish/static/js/2.a702034c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/foodFetish/static/js/2.a702034c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353a400376100f25f880",
+    "url": "/foodFetish/static/js/main.fe9b0071.chunk.js"
+  },
+  {
+    "revision": "999313dec8c5817b2a19",
+    "url": "/foodFetish/static/js/runtime-main.7ca9ae64.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/foodFetish/static/media/persik.4e1ec840.png"
+  }
+]);
